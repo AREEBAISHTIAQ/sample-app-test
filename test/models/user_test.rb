76aac9a5276bb40ce0b6abfbd5ff9f7b57f12e47
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-08-06 16:22:49 +0500

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-      password: "foobar", password_confirmation: "foobar")
+                    password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -72,5 +72,6 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
 
 end
