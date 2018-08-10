3a391aed970d5bbc7ccdd2b4169c45249766343a
AREEBISHTIAQ <areeba.aic@gmail.com>
2018-08-10 18:00:14 +0500

    last try

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@example.com'
+  default from: 'junk-app.herokuapp.com'
   layout 'mailer'
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,7 +30,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
       test "login with remembering" do
         log_in_as(@user, remember_me: '1')
-        assert_equal FILL_IN, assigns(:user).FILL_IN
+        assert_equal @user, assigns(:user)
       end
     
       test "login without remembering" do
