0efad69523484d529d09ff2eb6b764525895d59e
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-08-26 18:51:55 +0500

    improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
- end
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
